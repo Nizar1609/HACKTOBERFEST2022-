3b074bcfdb89e51acb5b65b00cb826a2c4bb6147
Nizar1609 <115543866+Nizar1609@users.noreply.github.com>
2022-10-23 19:09:37 +0700

    convert data tipe bool,int,str

---
+++ convertDataTpe.py
@@ -0,0 +1,22 @@
+# tipe data
+
+# tipe data integer (int)
+
+data_integer = 10
+
+print("nilai data :",data_integer , type(data_integer))
+
+# tipe data string (str)
+
+data_string = "nizar mahendra"
+print("nilai data:",data_string, type(data_string))
+
+
+# tipe data float 
+data_float = 110.8
+print("nilai data:",data_float, type(data_float))
+
+# tipe data booelan (bool)
+data_bool = True
+
+print("nilai data:",data_bool, type(data_bool))
